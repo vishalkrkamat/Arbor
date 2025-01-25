91f2ed8d2999394adf4854e1620d0b958125fb1c
vishalkrkamat <vishalkrkamat7@gmail.com>
2025-01-25 15:32:15 +0530

    walkdir used for listing dir

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,19 @@
 use std::fs;
 use std::io;
-
+use walkdir::WalkDir;
 fn main() -> io::Result<()> {
     let path = ".";
-    for entry in fs::read_dir(path)? {
-        let entry = entry?;
-        let path = entry.path();
-        if path.is_dir() {
-            let mut n = String::new();
-            println!("the directory {}", path.display());
-            io::stdin().read_line(&mut n);
-            if n.trim() == "n" {
-                for entry in fs::read_dir(path)? {
-                    let entry = entry?;
-                    let path = entry.path();
-                    if path.is_dir() {
-                        println!("The directory: {path:?}");
-                    }
+    for entry in WalkDir::new(path) {
+        match entry {
+            Ok(entry) => {
+                let path = entry.path();
+                if path.is_dir() {
+                    println!("Directory: {})", path.display());
+                } else {
+                    println!("File: {}", path.display())
                 }
             }
-        } else {
-            println!("The file {}", path.display())
+            Err(_e) => eprintln!("Erro"),
         }
     }
     Ok(())
