f89182cb5f08be1e524a1f5bd6a17dae25e9e783
vishalkrkamat <vishalkrkamat7@gmail.com>
2025-01-27 12:18:36 +0530

    listing file and directory:

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
 use std::fs;
 use std::io;
+use std::path::PathBuf;
 use walkdir::WalkDir;
 fn main() -> io::Result<()> {
-    let path = ".";
-    for entry in WalkDir::new(path) {
+    let path = "/home/vishal/";
+    //list_dir(path.to_string(), 1);
+    let mut vec: Vec<PathBuf> = Vec::new();
+    for entry in fs::read_dir(path)? {
         match entry {
             Ok(entry) => {
                 let path = entry.path();
-                if path.is_dir() {
-                    println!("Directory: {})", path.display());
-                } else {
-                    println!("File: {}", path.display())
-                }
+                vec.push(path);
             }
             Err(_e) => eprintln!("Erro"),
         }
     }
+    for path in &vec {
+        if path.is_dir() {
+            println!("Directory: {:?}", path);
+        } else {
+            println!("File: {:?}", path);
+        }
+    }
+
     Ok(())
+}
+
+fn list_dir(path: String, dept: u8) {
+    let path = path;
+    for entry in WalkDir::new(path).max_depth(dept.into()) {
+        match entry {
+            Ok(entry) => {
+                println!("List{:?} ", entry)
+            }
+            Err(_e) => println!("The erro"),
+        }
+    }
 }
