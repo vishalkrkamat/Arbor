6a93876d03c915524280c4f02ef896e3b5b9b4aa
vishalkrkamat <vishalkrkamat7@gmail.com>
2025-01-27 22:34:08 +0530

    added tying to implement

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,35 @@
-use std::fs;
-use std::io;
-use std::path::PathBuf;
+use crossterm::terminal;
+use crossterm::{
+    event::{read, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, ClearType},
+};
+use std::{fs, io, path::PathBuf};
 use walkdir::WalkDir;
+fn clear() {
+    execute!(std::io::stdout(), terminal::Clear(ClearType::All)).unwrap();
+}
+
 fn main() -> io::Result<()> {
+    clear();
+    // start of terminal mode
+    enable_raw_mode().expect("Failed to enter raw mode");
+
+    loop {
+        match read().unwrap() {
+            Event::Key(event) => match event.code {
+                KeyCode::Char('k') => println!("Up "),
+                KeyCode::Char('j') => println!("Down"),
+                KeyCode::Char('q') => break,
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+
+    disable_raw_mode().expect("Failed to enter raw mode");
+    // End of terminal mode
+
     let path = "/home/vishal/";
     //list_dir(path.to_string(), 1);
     let mut vec: Vec<PathBuf> = Vec::new();
