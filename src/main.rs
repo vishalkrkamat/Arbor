0fab803953499957cc9bcd67cbfa6f88dc82d4dd
vishalkrkamat <vishalkrkamat7@gmail.com>
2025-01-28 08:12:44 +0530

    directory highlighting

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,33 @@ use crossterm::terminal;
 use crossterm::{
     event::{read, Event, KeyCode},
     execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
     terminal::{disable_raw_mode, enable_raw_mode, ClearType},
 };
-use std::{fs, io, path::PathBuf};
+use std::{fs, io, io::Write, path::PathBuf};
 use walkdir::WalkDir;
 fn clear() {
     execute!(std::io::stdout(), terminal::Clear(ClearType::All)).unwrap();
 }
 
 fn main() -> io::Result<()> {
-    clear();
+    //  clear();
     // start of terminal mode
-    enable_raw_mode().expect("Failed to enter raw mode");
+    //    enable_raw_mode().expect("Failed to enter raw mode");
+    //
+    //   loop {
+    //      match read().unwrap() {
+    //         Event::Key(event) => match event.code {
+    //            KeyCode::Char('k') => println!("Up "),
+    //           KeyCode::Char('j') => println!("Down"),
+    //          KeyCode::Char('q') => break,
+    //         _ => {}
+    //    },
+    //   _ => {}
+    //}
+    //}
 
-    loop {
-        match read().unwrap() {
-            Event::Key(event) => match event.code {
-                KeyCode::Char('k') => println!("Up "),
-                KeyCode::Char('j') => println!("Down"),
-                KeyCode::Char('q') => break,
-                _ => {}
-            },
-            _ => {}
-        }
-    }
-
-    disable_raw_mode().expect("Failed to enter raw mode");
+    //disable_raw_mode().expect("Failed to enter raw mode");
     // End of terminal mode
 
     let path = "/home/vishal/";
@@ -42,14 +43,29 @@ fn main() -> io::Result<()> {
             Err(_e) => eprintln!("Erro"),
         }
     }
-    for path in &vec {
+    for mut path in &vec {
         if path.is_dir() {
-            println!("Directory: {:?}", path);
+            //println!("Directory: {:?}", path);
+            let _ = highlight(path);
         } else {
             println!("File: {:?}", path);
         }
     }
 
+    Ok(())
+}
+
+//Highlight the directory
+fn highlight(path: &PathBuf) -> io::Result<()> {
+    let mut stdout = io::stdout();
+    execute!(
+        stdout,
+        SetForegroundColor(Color::Red),
+        SetBackgroundColor(Color::Black),
+        Print(path.to_string_lossy()),
+        ResetColor
+    );
+    writeln!(stdout)?;
     Ok(())
 }
 
