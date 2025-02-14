ef2e2191cc14e832588e5bf918b7e53e56d37c57
vishalkrkamat <vishalkrkamat7@gmail.com>
2025-02-14 12:12:33 +0530

    skeleton layout initial

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,86 @@
-use crossterm::terminal;
-
+use crossterm::event::{self, Event, KeyCode};
 use crossterm::{
-    event::{read, Event, KeyCode},
     execute,
     style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-    terminal::{
-        disable_raw_mode, enable_raw_mode, ClearType, EnterAlternateScreen, LeaveAlternateScreen,
-    },
 };
-
-use ratatui::Terminal;
 use ratatui::{
-    backend::CrosstermBackend,
-    layout::{Constraint, Direction, Layout},
-    style::Style,
-    text::{Line, Span},
-    widgets::{Block, Borders, List, ListItem},
+    layout::{Constraint, Layout, Rect},
+    style::{Style, Stylize},
+    widgets::{Block, List, ListDirection, ListItem, Paragraph},
+    DefaultTerminal, Frame,
 };
+use std::io::Error;
 use std::{fs, io, io::Write, path::PathBuf};
 use walkdir::WalkDir;
-fn clear() {
-    execute!(std::io::stdout(), terminal::Clear(ClearType::All)).unwrap();
-}
 
 fn main() -> io::Result<()> {
-    //  clear();
-    // start of terminal mode
-    enable_raw_mode().expect("Failed to enter raw mode");
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen).expect("Error creating alternate screen");
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend);
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+    result
+}
 
-    //   loop {
-    //      match read().unwrap() {
-    //         Event::Key(event) => match event.code {
-    //            KeyCode::Char('k') => println!("Up "),
-    //           KeyCode::Char('j') => println!("Down"),
-    //          KeyCode::Char('q') => break,
-    //         _ => {}
-    //    },
-    //   _ => {}
-    //}
-    //}
-
-    disable_raw_mode()?;
-    execute!(terminal?.backend_mut())?;
-
-    let path = ls("/home/vishal/".to_string());
-
-    // Create ListItems
-    let items: Vec<ListItem> = path
-        .iter()
-        .map(|file| ListItem::new(Line::from(Span::styled(*file, Style::default()))))
-        .collect();
-    //list_dir(path.to_string(), 1);
-    // let mut vec: Vec<PathBuf> = Vec::new();
-    //for entry in fs::read_dir(path)? {
-    //   match entry {
-    //      Ok(entry) => {
-    //         let path = entry.path();
-    //        vec.push(path);
-    //    }
-    //       Err(_e) => eprintln!("Erro"),
-    //}
-    //}
-    //for mut path in &vec {
-    //   if path.is_dir() {
-    //      //println!("Directory: {:?}", path);
-    //     let _ = highlight(path);
-    //} else {
-    //   println!("File: {:?}", path);
-    //}
-    //}
-
+fn run(mut terminal: DefaultTerminal) -> Result<(), Error> {
+    loop {
+        terminal.draw(render)?;
+        if let Event::Key(key) = event::read()? {
+            if key.code == KeyCode::Char('q') {
+                break;
+            }
+        }
+    }
     Ok(())
+}
+fn render(frame: &mut Frame) {
+    let fed = ls("/".to_string());
+    println!("{fed:?}");
+    let mainlay = Layout::vertical([
+        Constraint::Length(1),
+        Constraint::Min(1),
+        Constraint::Length(1),
+    ])
+    .split(frame.area());
+    let conte = Layout::horizontal([
+        Constraint::Percentage(30),
+        Constraint::Percentage(40),
+        Constraint::Percentage(30),
+    ])
+    .split(mainlay[1]);
+
+    let chared = ["hell", "heleddd", "ok df", "hekfd", "jfkej"];
+    let chare2 = ["file", "file3", "file4", "file5", "file6"];
+
+    let chare3 = ["file", "file3", "hello", "file4", "file5", "file6"];
+    let list = List::new(chared)
+        .block(Block::bordered().title("List"))
+        .style(Style::new().white())
+        .highlight_style(Style::new().italic())
+        .highlight_symbol(">>")
+        .repeat_highlight_symbol(true)
+        .direction(ListDirection::TopToBottom);
+
+    let list2 = List::new(chare2)
+        .block(Block::bordered().title("List"))
+        .style(Style::new().white())
+        .highlight_style(Style::new().italic())
+        .highlight_symbol(">>")
+        .repeat_highlight_symbol(true)
+        .direction(ListDirection::TopToBottom);
+
+    //let item = ListItem::new(chared[2]);
+    let list3 = List::new(chare3)
+        .block(Block::bordered().title("List"))
+        .style(Style::new().white())
+        .highlight_style(Style::new().italic())
+        .highlight_symbol(">>")
+        .repeat_highlight_symbol(true)
+        .direction(ListDirection::TopToBottom);
+
+    //frame.render_widget(greetin, frame.area());
+    frame.render_widget(Block::new().title("Header"), mainlay[0]);
+    frame.render_widget(list, conte[0]);
+    frame.render_widget(list2, conte[1]);
+    frame.render_widget(list3, conte[2]);
 }
 
 fn ls(path: String) -> io::Result<String> {
